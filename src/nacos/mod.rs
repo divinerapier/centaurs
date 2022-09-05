c15a26264c026015ab965631a9b62a8c0dcbe313
divinerapier <sihao.fang@outlook.com>
2022-09-05 17:11:30 +0800

    feat(servicediscovery): add errors and remove nacos from service discovery
    Signed-off-by: divinerapier <sihao.fang@outlook.com>

--- src/nacos/mod.rs
+++ src/nacos/mod.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{borrow::Cow, sync::Arc};
 
 use nacos_rust_client::client::{
     config_client::ConfigKey,
@@ -8,6 +8,8 @@ use nacos_rust_client::client::{
 
 use crate::servicediscovery::Instance;
 
+pub mod servicediscovery;
+
 pub struct API {
     config: ConfigClient,
     naming: Arc<NamingClient>,
@@ -16,49 +18,85 @@ pub struct API {
     group: String,
 }
 
-impl Default for API {
-    fn default() -> Self {
-        let server = std::env::var("NACOS_SERVER").unwrap_or_else(|_| "nacos".to_string());
-        let port = std::env::var("NACOS_PORT")
-            .unwrap_or_else(|_| "8848".to_string())
-            .parse::<u16>()
-            .unwrap_or(8848);
-        let host = HostInfo::parse(&format!("{server}:{port}"));
-        let tenant = std::env::var("NACOS_NAMESPACE").unwrap_or_default();
-        let data_id = std::env::var("NACOS_DATA_ID").unwrap();
-        let group = std::env::var("NACOS_GROUP").unwrap_or_else(|_| "DEFAULT_GROUP".to_string());
-        let server_name = std::env::var("KUBERNETES_SERVICE_NAME").unwrap();
-        tracing::debug!("NACOS_SERVER: {}", server);
-        tracing::debug!("NACOS_PORT: {}", port);
-        tracing::debug!("NACOS_NAMESPACE: {}", tenant);
-        tracing::debug!("NACOS_DATA_ID: {}", data_id);
-        tracing::debug!("NACOS_GROUP: {}", group);
-        tracing::debug!("KUBERNETES_SERVICE_NAME: {}", server_name);
+#[derive(derive_builder::Builder)]
+pub struct Config {
+    server: String,
+    port: u16,
+    tenant: String,
+    data_id: String,
+    group: String,
+    server_name: String,
+}
 
-        let naming = NamingClient::new(HostInfo::new(&server, port as u32), server_name);
-        let config = ConfigClient::new(host, tenant.clone());
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("find environment variable: {0}. {1}")]
+    VarNotFound(Cow<'static, str>, std::env::VarError),
+
+    #[error("parse nacos port: {0}")]
+    ParsePort(String),
+
+    #[error("nacos is disabled")]
+    Disable,
+}
+
+impl Config {
+    pub fn from_env() -> Result<Config, Error> {
+        std::env::var("NACOS_CONFIG_ENABLED")
+            .unwrap_or_default()
+            .parse::<bool>()
+            .ok()
+            .filter(|x| *x)
+            .ok_or(Error::Disable)?;
+
+        let server = Self::read_env("NACOS_SERVER")?;
+        let port = Self::read_env("NACOS_PORT")
+            .map(|v| v.parse::<u16>().unwrap_or(8848))
+            .unwrap_or(8848);
+        let tenant = Self::read_env("NACOS_NAMESPACE").unwrap_or_default();
+        let data_id = Self::read_env("NACOS_DATA_ID")?;
+        let group = Self::read_env("NACOS_GROUP").unwrap_or_else(|_| "DEFAULT_GROUP".to_string());
+        let server_name = Self::read_env("KUBERNETES_SERVICE_NAME")?;
+        Ok(Config {
+            server,
+            port,
+            tenant,
+            data_id,
+            group,
+            server_name,
+        })
+    }
+    fn read_env(env: &'static str) -> Result<String, Error> {
+        std::env::var(env).map_err(|e| Error::VarNotFound(Cow::Borrowed(env), e))
+    }
+
+    pub fn into_api(self) -> API {
+        API::new(self)
+    }
+}
+
+impl API {
+    pub fn new(cfg: Config) -> Self {
+        let server = cfg.server;
+        let port = cfg.port;
+        let host = HostInfo::new(&server, port as u32);
+        let naming = NamingClient::new(host.clone(), cfg.server_name);
+        let config = ConfigClient::new(host, cfg.tenant.clone());
 
         API {
             naming,
             config,
-            tenant,
-            data_id,
-            group,
+            tenant: cfg.tenant,
+            data_id: cfg.data_id,
+            group: cfg.group,
         }
     }
 }
 
 impl API {
     /// new_from_env 通过环境变量初始化
-    pub fn new_from_env() -> Option<API> {
-        let enable = std::env::var("NACOS_CONFIG_ENABLED")
-            .unwrap_or_default()
-            .parse::<bool>()
-            .unwrap_or_default();
-        if !enable {
-            return None;
-        }
-        Some(API::default())
+    pub fn new_from_env() -> Result<API, Error> {
+        Ok(Config::from_env()?.into_api())
     }
 
     pub async fn load(&self) -> Result<String, Box<dyn std::error::Error + Send>> {
@@ -94,9 +132,9 @@ impl API {
 }
 
 /// register_factory 注册初始化函数到 servicediscovery::FACTORIES
-pub fn register_factory() {
-    crate::servicediscovery::register_factory(|| {
+pub fn register_factory() -> Result<(), crate::servicediscovery::Error> {
+    crate::servicediscovery::register_factory("nacos".to_string(), || {
         let nacos = API::new_from_env()?;
-        Some(Box::new(nacos))
+        Ok(Box::new(nacos))
     })
 }
--- src/servicediscovery/mod.rs
+++ src/servicediscovery/mod.rs
@@ -1,25 +1,16 @@
-#[cfg(feature = "nacos-servicediscovery")]
-pub mod nacos;
+use std::{collections::HashMap, sync::Mutex};
 
-use std::sync::Mutex;
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("duplicated register with name: {0}")]
+    DuplicatedRegister(String),
+}
 
+#[derive(derive_builder::Builder)]
 pub struct Instance {
     pub ip: String,
     pub port: u32,
     pub service_name: String,
-}
-
-impl Instance {
-    pub fn new<S>(ip: S, port: u32, service_name: String) -> Option<Instance>
-    where
-        S: Into<Option<String>>,
-    {
-        Some(Instance {
-            ip: ip.into()?,
-            port,
-            service_name,
-        })
-    }
 }
 
 pub trait Registry: Send {
@@ -52,22 +43,32 @@ impl Drop for RegisterGuard {
     }
 }
 
-pub type Factory = fn() -> Option<Box<dyn Registry>>;
+pub type Factory = fn() -> Result<Box<dyn Registry>, Box<dyn std::error::Error>>;
 
 lazy_static::lazy_static! {
-    static ref FACTORIES: Mutex<Vec<Factory>> =  Mutex::new(Vec::new());
+    static ref FACTORIES: Mutex<HashMap <String,Factory>> =  Mutex::new(HashMap::new());
 }
 
-pub fn register_factory(f: Factory) {
+pub fn register_factory(name: String, f: Factory) -> Result<(), Error> {
     let mut factories = FACTORIES.lock().unwrap();
-    factories.push(f);
+    factories
+        .get(&name)
+        .ok_or_else(|| Error::DuplicatedRegister(name.clone()))?;
+    factories.insert(name, f);
+    Ok(())
 }
 
 pub fn init() -> Option<Discovery> {
     let registries = FACTORIES.lock().unwrap();
-    for factory in registries.iter() {
-        if let Some(registry) = factory() {
-            return Some(Discovery { registry });
+    for (name, factory) in registries.iter() {
+        match factory() {
+            Ok(registry) => {
+                tracing::info!("use register: {}", name);
+                return Some(Discovery { registry });
+            }
+            Err(e) => {
+                tracing::warn!("register {} with error {}", name, e);
+            }
         }
     }
     None
